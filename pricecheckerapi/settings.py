c81a68a53650d293f711f42f4939942a2ce1b37b
chrismayeslobo <76921717+chrismayeslobo@users.noreply.github.com>
2025-03-24 13:04:00 -0700

    fixed postgres connection env var

--- pricecheckerapi/settings.py
+++ pricecheckerapi/settings.py
@@ -82,11 +82,7 @@ WSGI_APPLICATION = 'pricecheckerapi.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
+    'default': dj_database_url.parse(config('DATABASE_URL'))
 }
 
 # Password validation
